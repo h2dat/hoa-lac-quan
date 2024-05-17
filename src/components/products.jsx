ad8f263615f38cbf7d30d214605e9ea7f0c76d2a
HidroDatz <hidrodat2002@gmail.com>
2024-05-17 20:21:10 +0700

    add new banner

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,10 +1,21 @@
 import { Image } from "./image";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
 import React from "react";
 
 export const Products = (props) => {
-  const handleItemClick = (productId) => {
-    localStorage.setItem('productId', productId);
+  const navigate = useNavigate();
+
+  const handleItemClick = (e, product) => {
+    e.preventDefault()
+    if (product.status === 'comming_soon') {
+      alert("Sản phẩm này hiện chưa phục vụ")
+    }
+    else {
+      localStorage.setItem('productId', product.id)
+      navigate('/detail');
+
+    }
   };
   return (
     <div id="products" className="text-center">
@@ -12,25 +23,25 @@ export const Products = (props) => {
         <div className="section-title">
           <h2>Products</h2>
           <p>
-          Nốt Hương Đặc Sản – Nguyên Bản – Thủ Công 
+            Nốt Hương Đặc Sản – Nguyên Bản – Thủ Công
           </p>
         </div>
         <div className="row">
           <div className="products-items">
             {props.data
               ? props.data.map((item, i) => (
-                  <div
-                    key={item.id}
-                    className="col-sm-6 col-md-4 col-lg-4"
-                  >
-                     <Link to={'/detail'} onClick={() =>{ handleItemClick(item.id)}}>
+                <div
+                  key={item.id}
+                  className="col-sm-6 col-md-4 col-lg-4"
+                >
+                  <Link to={'/detail'} onClick={(e) => { handleItemClick(e, item) }}>
                     <Image
                       title={item.name}
                       largeImage={item.Image[0]}
                     />
                   </Link>
-                  </div>
-                ))
+                </div>
+              ))
               : "Loading..."}
           </div>
         </div>
